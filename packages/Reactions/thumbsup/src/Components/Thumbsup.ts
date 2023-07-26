d5cc05dede4724e74d47440978618c413aad2285
Rameesa <prameesa96@gmail.com>
2023-07-26 17:12:33 +0530

    chore: update form package

--- packages/Reactions/thumbsup/src/Components/Thumbsup.ts
+++ packages/Reactions/thumbsup/src/Components/Thumbsup.ts
@@ -1,13 +1,10 @@
 export class Thumbsup extends HTMLElement {
-    count = null;
+    count = '';
     names = '';
     isHovered = false;
     envInfo = {};
     constructor() {
         super();
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-        this.handleMouseLeave = this.handleMouseLeave.bind(this);
-        this.handleClick = this.handleClick.bind(this);
     }
 
     connectedCallback() {
@@ -54,16 +51,6 @@ this.render()
       console.log(error);
     });
 }
-    handleMouseEnter() {
-        this.isHovered = true;
-        this.render();
-    }
-
-    handleMouseLeave() {
-        this.isHovered = false;
-        this.render();
-    }
-
     handleClick() {
       console.log("clicked")
       const query = this.getAttribute('query')
@@ -101,9 +88,7 @@ this.render()
         if(this.shadowRoot){
         this.shadowRoot.innerHTML = `
       <div>
-        <span class="slds-m-right_x-small slds-m-right_x-bottom" style="position: relative"
-          onMouseEnter="${this.handleMouseEnter}"
-          onMouseLeave="${this.handleMouseLeave}">
+        <span class="slds-m-right_x-small slds-m-right_x-bottom" style="position: relative">
           <button id="thumbsup-button"  style="border: none; cursor: pointer" class="tooltip-info-link social-proof-emoji thumbsup_click checked" title="thumbsup" id="thumbsup_click" data-emoji="thumbsup">
             <span class="slds-badge font-size-14 thumbsup_Button" id="thumbsup_Button"> Follow 👍
               <span class="slds-m-left_xx-small thumbsup_emojiCount" id="thumbsup_emojiCount">${this.count}</span>

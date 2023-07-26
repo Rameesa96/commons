72bff1e44803cc14331ff93943854bdf77431f31
Rameesa <prameesa96@gmail.com>
2023-07-26 20:26:22 +0530

    chore: update reaction package

--- packages/Reactions/thumbsup/src/Components/Thumbsup.ts
+++ packages/Reactions/thumbsup/src/Components/Thumbsup.ts
@@ -53,25 +53,29 @@ this.render()
       console.log(error);
     });
 }
-  private handleClick() {
-      console.log("clicked")
-      const query = this.getAttribute('query')
-      const bearerToken = this.getAttribute('bearerToken')||""
-      const authAPIUrl = this.getAttribute('authAPIUrl')||""
-      console.log(authAPIUrl)
-      const data = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "cache-control": "no-cache",
-          "auth-strategy": "next",
-          "Authorization": bearerToken
-        },
-        body: JSON.stringify({
-          query: query
-        })
+  private handleClick(event: Event) {
+    event.preventDefault();
+    console.log("clicked");
 
-      }
+    const query = this.getAttribute('query') || "";
+    const bearerToken = this.getAttribute('bearerToken') || "";
+    const authAPIUrl = this.getAttribute('authAPIUrl') || "";
+    console.log("authAPIUrl:", authAPIUrl);
+    console.log("query:", query);
+
+    const data = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "cache-control": "no-cache",
+        "auth-strategy": "next",
+        "Authorization": bearerToken
+      },
+      body: JSON.stringify({
+        query: query
+      })
+    };
+
     fetch(authAPIUrl, data)
       .then(response => {
         if (!response.ok) {
@@ -80,12 +84,13 @@ this.render()
         return response.json();
       })
       .then(data => {
-        console.log(data);
+        console.log("Response data:", data);
       })
       .catch(error => {
         console.log('Error:', error);
       });
-     }
+  }
+
 
     private render() {
         if(this.shadowRoot){

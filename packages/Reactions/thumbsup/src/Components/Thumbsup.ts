c98f0e46dea42dbfa87bce6a636bdbc9eaad02de
Rameesa <prameesa96@gmail.com>
2023-07-28 16:47:59 +0530

    chore: update thumbsup reaction

--- packages/Reactions/thumbsup/src/Components/Thumbsup.ts
+++ packages/Reactions/thumbsup/src/Components/Thumbsup.ts
@@ -1,4 +1,4 @@
-type UserData ={
+type UserData = {
   createdat: string;
   current_timestamp: string;
   emojiName: string;
@@ -6,126 +6,117 @@ type UserData ={
   // Add other properties here...
   isAdmin: boolean;
   personName: string;
-}
+};
 export class Thumbsup extends HTMLElement {
-  
-    count:number|string = "";
-    names = '';
-    isHovered = false;
-    envInfo = {};
-   bearerToken=""
-   authAPIUrl=""
-   query=""
-   
-   
-    constructor() {
-        super();
-      this.attachShadow({ mode: 'open' });
-    }
+  count: number | string = "";
+  names = "";
+  isHovered = false;
+  envInfo = {};
+  bearerToken = "";
+  authAPIUrl = "";
+  query = "";
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
   async connectedCallback() {
-    this.query = this.getAttribute('query')||"";
-    this.bearerToken = this.getAttribute('bearerToken') || "";
-    this.authAPIUrl = this.getAttribute('authAPIUrl')||""
+    this.query = this.getAttribute("query") || "";
+    this.bearerToken = this.getAttribute("bearerToken") || "";
+    this.authAPIUrl = this.getAttribute("authAPIUrl") || "";
     this.handleClick = this.handleClick.bind(this);
     await this.getData();
     this.render();
-    const cancelButton = this.shadowRoot?.querySelector('#cancel-button');
-    console.log(cancelButton)
-    if (cancelButton) {
-    cancelButton.addEventListener('click', this.handleClick);
+    const Button = this.shadowRoot?.querySelector("#button");
+    console.log(Button);
+    if (Button) {
+      Button.addEventListener("click", this.handleClick);
     }
   }
 
-  private async getData(){
-  const queryGetdata = this.getAttribute('queryGetdata')
-  console.log("beare", this.bearerToken)
-  console.log("authAPIUrl:", this.authAPIUrl);
-  console.log("query:",this.query);
-  const authAPIUrlGetdata= this.getAttribute('authAPIUrlGetdata')
-  if (!authAPIUrlGetdata) {
-    console.error('authAPIUrlGetdata is null.');
-    return;
-  }
-  const data = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "cache-control": "no-cache"
-    },
-    body: JSON.stringify({
-      query: queryGetdata
-    })
-  };
-
- await fetch(authAPIUrlGetdata, data)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      
-      if (data.data.getPromData.status === "Success") {
-        const JSONData = JSON.parse(data.data.getPromData.data) as UserData[];
-        // Your code to handle JSONData here
-        if (JSONData.length > 0) {
-          this.count = JSONData.length
-          const nonAdminUsers = JSONData.filter(user=> !user.isAdmin);
-          const nonAdminUserNames = nonAdminUsers.map(user=> user.personName).join(', ');
-          this.names = nonAdminUserNames;
-        }
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-    this.render()
-}
- 
-  private handleClick() {
-    console.log("beareh", this.bearerToken)
-    console.log("authAPIUrlh:",this.authAPIUrl);
-    console.log("queryh:", this.query);
-    console.log("clicked");
-    if (!this.authAPIUrl) {
-      console.error('authAPIUrlGetdata is null.');
+  private async getData() {
+    const queryGetdata = this.getAttribute("queryGetdata");
+    const authAPIUrlGetdata = this.getAttribute("authAPIUrlGetdata");
+    if (!authAPIUrlGetdata) {
       return;
     }
- 
+    const data = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "cache-control": "no-cache",
+      },
+      body: JSON.stringify({
+        query: queryGetdata,
+      }),
+    };
+
+    await fetch(authAPIUrlGetdata, data)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data.getPromData.status === "Success") {
+          const JSONData = JSON.parse(data.data.getPromData.data) as UserData[];
+          // Your code to handle JSONData here
+          if (JSONData.length > 0) {
+            this.count = JSONData.length;
+            const nonAdminUsers = JSONData.filter((user) => !user.isAdmin);
+            const nonAdminUserNames = nonAdminUsers
+              .map((user) => user.personName)
+              .join(", ");
+            this.names = nonAdminUserNames;
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    this.render();
+  }
+
+  private handleClick() {
+    if (!this.authAPIUrl) {
+      return;
+    }
+
     const data = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "cache-control": "no-cache",
         "auth-strategy": "next",
-        "Authorization": this.bearerToken
+        Authorization: this.bearerToken,
       },
       body: JSON.stringify({
-        query: this.query
-      })
+        query: this.query,
+      }),
     };
 
     fetch(this.authAPIUrl, data)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok.');
+          throw new Error("Network response was not ok.");
         }
         return response.json();
       })
-      .then(data => {
-        if (data.data.addPrometheusData.addPrometheusDataResult.status==="Success"){
+      .then((data) => {
+        if (
+          data.data.addPrometheusData.addPrometheusDataResult.status ===
+          "Success"
+        ) {
           setTimeout(() => {
             this.getData(); // Call getData after a delay
           }, 5000);
         }
       })
-      .catch(error => {
-        console.log('Error:', error);
+      .catch((error) => {
+        console.log("Error:", error);
       });
   }
 
-
-    private render() {
-        if(this.shadowRoot){
-        this.shadowRoot.innerHTML = `
+  private render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
         <style>
 .text{
   color:#080707;
@@ -162,7 +153,7 @@ button:hover + .tooltip-info {
         </style>
       <div>
         <span" class="button" style="position: relative">
-          <button type="button"  id="cancel-button" >
+          <button type="button"  id="button" >
           <span class="text">Follow 👍
           <span class="count" thumbsup_emojiCount" id="thumbsup_emojiCount">${this.count}</span></span>
           </button>
@@ -174,8 +165,8 @@ button:hover + .tooltip-info {
         </div>
       </div>
     `;
-}
     }
+  }
 }
 
-customElements.define('thumbs-up', Thumbsup);
+customElements.define("thumbs-up", Thumbsup);
